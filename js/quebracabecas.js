0bfac3d816c42c9fe541265515f89becf2cba9be
zelionforge <zelionforge@gmail.com>
2026-01-29 15:15:14 -0300

    Update quebracabecas.js

--- js/quebracabecas.js
+++ js/quebracabecas.js
@@ -2,11 +2,11 @@ document.querySelectorAll(".qb-carousel").forEach(carousel => {
   const images = carousel.querySelectorAll("img");
   let index = 0;
 
-  images[0].classList.add("active");
+  images[index].classList.add("active");
 
   setInterval(() => {
     images[index].classList.remove("active");
     index = (index + 1) % images.length;
     images[index].classList.add("active");
-  }, 2500);
+  }, 2500); // troca de imagem a cada 2.5s
 });
